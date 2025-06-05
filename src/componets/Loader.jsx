f4c008f9866ba0f77dcc1b8f56fea09d629d5b85
JJcode404 <khamjapher1@gmail.com>
2025-06-05 15:12:10 +0300

    feat: update a loader

--- src/componets/Loader.jsx
+++ src/componets/Loader.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/Loader.module.css";
 export default function Loader() {
   return (
     <div className={styles.overlay}>
-      <div className={styles.spinner}></div>
+      <img src="/loadingState.svg" alt="loading" />
     </div>
   );
 }
